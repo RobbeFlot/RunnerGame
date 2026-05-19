7244b9f0f4f3d9cde8ff1baf0e97de5b93624b81
RobbeFlot <162193041+RobbeFlot@users.noreply.github.com>
2026-05-19 23:33:56 +0200

    Draw a texture to the screen
    This was a pain in the ass

--- RunnerGame/RunnerGame/main.cpp
+++ RunnerGame/RunnerGame/main.cpp
@@ -1,8 +1,16 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode({ 1000, 600 }), "Runner Game!");
+
+	sf::Texture SteveText;
+	if (!SteveText.loadFromFile("Assets/STEVE.png")) {
+		std::cout << "Failed to load texture!";
+		return -1;
+	}
+	sf::Sprite STEVE(SteveText);
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
@@ -10,5 +18,9 @@ int main()
 				window.close();
 			}
 		}
+
+		window.clear(sf::Color::Black);
+		window.draw(STEVE);
+		window.display();
 	}
 }
