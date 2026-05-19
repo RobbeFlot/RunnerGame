acc14b83752230a720f780cef27b36233481e645
RobbeFlot <162193041+RobbeFlot@users.noreply.github.com>
2026-05-19 22:38:16 +0200

    Window creation

--- RunnerGame/RunnerGame/main.cpp
+++ RunnerGame/RunnerGame/main.cpp
@@ -2,5 +2,13 @@
 
 int main()
 {
-	
+	sf::RenderWindow window(sf::VideoMode({ 1000, 600 }), "Runner Game!");
+
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<sf::Event::Closed>()) {
+				window.close();
+			}
+		}
+	}
 }
