3d19872ecee56608582b5f9ec77a2b0c84ee59a8
RobbeFlot <162193041+RobbeFlot@users.noreply.github.com>
2026-05-20 16:19:52 +0200

    Sprite loading function
    Created a function to load sprites obviously this came with very fun to resolve errors, but i fixed them now, at the cost of my sanity

--- RunnerGame/RunnerGame/main.cpp
+++ RunnerGame/RunnerGame/main.cpp
@@ -1,10 +1,25 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <map>
+
+sf::Sprite& loadSprite(std::string textPath, std::string textName, std::vector<sf::Sprite*>& sprites, std::map<std::string, sf::Texture*>& textures) {
+	sf::Texture* texture = new sf::Texture();
+
+	textures[textName] = texture;
+	if (!texture->loadFromFile(textPath)) {
+		std::cout << "Failed to load sprite texture at path: " << textPath << '\n';
+	}
+
+	sf::Sprite* sprite = new sf::Sprite(*texture);
+	sprites.push_back(sprite);
+	return *sprite;
+}
 
 void drawGame(sf::RenderWindow& window, std::vector<sf::Sprite*>& sprites) {
 	window.clear(sf::Color::Blue);
 	for (sf::Sprite* sprite : sprites) {
+		std::cout << sprite;
 		window.draw(*sprite);
 	}
 	window.display();
@@ -15,14 +30,9 @@ int main()
 	sf::RenderWindow window(sf::VideoMode({ 1000, 600 }), "Runner Game!");
 
 	std::vector<sf::Sprite*> sprites;
+	std::map<std::string, sf::Texture*> textures;
 
-	sf::Texture SteveText;
-	if (!SteveText.loadFromFile("Assets/STEVE.png")) {
-		std::cout << "Failed to load texture!";
-		return -1;
-	}
-	sf::Sprite STEVE(SteveText);
-	sprites.push_back(&STEVE);
+	sf::Sprite Steve = loadSprite("Assets/STEVE.png", "STEVE", sprites, textures);
 
 	while (window.isOpen()) {
 		while (const std::optional event = window.pollEvent()) {
